449db0b3861b052a784fc04b716bcc06b257c357
Po-Hsiang Huang <miterfrants@gmail.com>
2015-06-25 00:51:35 +0800

    print url

--- src/test/java/WebDriverWithHelperArraysTest.java
+++ src/test/java/WebDriverWithHelperArraysTest.java
@@ -155,8 +155,9 @@ public class WebDriverWithHelperArraysTest implements SauceOnDemandSessionIdProv
                 String browserName = String.format("%-19s", browser).replaceAll(" ", ".").replaceFirst("[.]", " ");
                 String browserVer = String.format("%-19s", browserVersion).replaceAll(" ", ".");
                 System.out.println("@Test validateTitle() testing browser/version: " + browserName + browserVer + "platform: " + platform);
-
-                webDriver.get("http://hahow.csie.org");
+                String url = "http://hahow.csie.org";
+                System.out.println(url);
+                webDriver.get(url);
                 assertEquals("I am a page title - Sauce Labs", webDriver.getTitle());
 
             } catch (Exception e) {
