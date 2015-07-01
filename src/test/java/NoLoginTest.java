31a2f6e4171fb78771b2368c4d20c32dbe913598
Po-Hsiang Huang <miterfrants@gmail.com>
2015-07-02 02:34:46 +0800

    no message

---
+++ src/test/java/NoLoginTest.java
@@ -0,0 +1,29 @@
+public class NoLoginTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        // Choose the browser, version, and platform to test
+        DesiredCapabilities capsIE = DesiredCapabilities.internetExplorer();
+        capsIE.setCapability("platform", "Windows 8.1");
+        capsIE.setCapability("version", "11.0");
+        // Create the connection to Sauce Labs to run the tests
+        this.driver = new RemoteWebDriver(
+                new URL("http://miterfrants:08d2200d-eabe-4d7e-817b-ecb7fb03af57@ondemand.saucelabs.com:80/wd/hub"),
+                capsIE);
+    }
+
+    @Test
+    public void webDriver() throws Exception {
+        // Make the browser get the page and check its title
+        driver.get("http://hahow.csie.org/");
+        assertEquals("分享，學習 - Hahow 好學校", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
