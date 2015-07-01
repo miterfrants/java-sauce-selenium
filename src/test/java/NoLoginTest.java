04791f488942643f387de680ba0ddb8a8abfe112
Po-Hsiang Huang <miterfrants@gmail.com>
2015-07-02 03:58:46 +0800

    no message

--- src/test/java/NoLoginTest.java
+++ src/test/java/NoLoginTest.java
@@ -58,7 +58,7 @@ public class NoLoginTest implements SauceOnDemandSessionIdProvider
 
         drivers.add(capsIE11Win7);
 
-        return Arrays.asList(drivers);
+        return drivers.toArray();
 
     }
 
