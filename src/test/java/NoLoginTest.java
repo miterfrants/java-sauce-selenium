05ab11b5de3e700ce0b00e8b125e5997f61169f5
Po-Hsiang Huang <miterfrants@gmail.com>
2015-07-02 11:36:45 +0800

    no message

--- src/test/java/NoLoginTest.java
+++ src/test/java/NoLoginTest.java
@@ -44,23 +44,96 @@ public class NoLoginTest implements SauceOnDemandSessionIdProvider
     public static Collection<Object[]> data() {
         System.out.println("set @parameters");
 
-        List<DesiredCapabilities> drivers = new ArrayList<DesiredCapabilities>();
-        // Choose the browser, version, and platform to test
+        //Win 8.1
         DesiredCapabilities capsIE11Win81 = DesiredCapabilities.internetExplorer();
         capsIE11Win81.setCapability("platform", "Windows 8.1");
         capsIE11Win81.setCapability("version", "11.0");
+        capsIE11Win81.setCapability("screenResolution", "1280x1024");
 
-        drivers.add(capsIE11Win81);
+        DesiredCapabilities capsChromeWin81 = DesiredCapabilities.chrome();
+        capsChromeWin81.setCapability("platform", "Windows 8.1");
+        capsChromeWin81.setCapability("version", "43.0");
+        capsChromeWin81.setCapability("screenResolution", "1280x1024");
+
+        DesiredCapabilities capsFirefoxWin81 = DesiredCapabilities.firefox();
+        capsFirefoxWin81.setCapability("platform", "Windows 8.1");
+        capsFirefoxWin81.setCapability("version", "38.0");
+        capsFirefoxWin81.setCapability("screen-resolution", "1280x1024");
+
+        //Win 7
 
         DesiredCapabilities capsIE11Win7 = DesiredCapabilities.internetExplorer();
         capsIE11Win7.setCapability("platform", "Windows 7");
         capsIE11Win7.setCapability("version", "11.0");
+        capsIE11Win7.setCapability("screen-resolution", "1280x1024");
 
-        drivers.add(capsIE11Win7);
+        DesiredCapabilities capsChromeWin7 = DesiredCapabilities.chrome();
+        capsChromeWin7.setCapability("platform", "Windows 7");
+        capsChromeWin7.setCapability("version", "43.0");
+        capsChromeWin7.setCapability("screen-resolution", "1280x1024");
+
+        DesiredCapabilities capsFirefox = DesiredCapabilities.firefox();
+        capsFirefox.setCapability("platform", "Windows 7");
+        capsFirefox.setCapability("version", "38.0");
+        capsFirefox.setCapability("screen-resolution", "1280x1024");
+
+        //Mac 10.10
+        DesiredCapabilities capsChromeMac1010 = DesiredCapabilities.chrome();
+        capsChromeMac1010.setCapability("platform", "OS X 10.10");
+        capsChromeMac1010.setCapability("version", "43.0");
+        capsChromeMac1010.setCapability("screenResolution", "1024x768");
+
+        DesiredCapabilities capsSafariMac1010 = DesiredCapabilities.safari();
+        capsSafariMac1010.setCapability("platform", "OS X 10.10");
+        capsSafariMac1010.setCapability("version", "8.0");
+        capsSafariMac1010.setCapability("screenResolution", "1024x768");
+
+        DesiredCapabilities capsFirefoxMac1010 = DesiredCapabilities.firefox();
+        capsFirefoxMac1010.setCapability("platform", "OS X 10.10");
+        capsFirefoxMac1010.setCapability("version", "38.0");
+        capsFirefoxMac1010.setCapability("screenResolution", "1024x768");
+
+        //iPhone
+        DesiredCapabilities capsIphone82 = DesiredCapabilities.iphone();
+        capsIphone82.setCapability("platform", "OS X 10.10");
+        capsIphone82.setCapability("version", "8.2");
+        capsIphone82.setCapability("deviceName","iPhone Simulator");
+        capsIphone82.setCapability("deviceOrientation", "portrait");
+
+        DesiredCapabilities capsIphone81 = DesiredCapabilities.iphone();
+        capsIphone81.setCapability("platform", "OS X 10.10");
+        capsIphone81.setCapability("version", "8.1");
+        capsIphone81.setCapability("deviceName","iPhone Simulator");
+        capsIphone81.setCapability("deviceOrientation", "portrait");
+
+        //iPad
+        DesiredCapabilities capsiPad = DesiredCapabilities.iphone();
+        capsiPad.setCapability("platform", "OS X 10.10");
+        capsiPad.setCapability("version", "8.2");
+        capsiPad.setCapability("deviceName","iPad Simulator");
+        capsiPad.setCapability("deviceOrientation", "portrait");
+
+        //Android
+        DesiredCapabilities capsAndroid44 = DesiredCapabilities.android();
+        capsAndroid44.setCapability("platform", "Linux");
+        capsAndroid44.setCapability("version", "4.4");
+        capsAndroid44.setCapability("deviceName","Google Nexus 7 HD Emulator");
+        capsAndroid44.setCapability("deviceOrientation", "portrait");
 
         Object[][] caps = {
             {capsIE11Win81},
-            {capsIE11Win7}
+            {capsChromeWin81},
+            {capsFirefoxWin81},
+            {capsIE11Win7},
+            {capsChromeWin7},
+            {capsFirefoxWin7},
+            {capsChromeMac1010},
+            {capsSafariMac1010},
+            {capsFirefoxMac1010},
+            {capsIphone82},
+            {capsIphone81},
+            {capsiPad},
+            {capsAndroid44}
         };
 
         return Arrays.asList(caps);
@@ -87,12 +160,6 @@ public class NoLoginTest implements SauceOnDemandSessionIdProvider
         driver.quit();
         System.out.println("job finished");
     }
-
-    @After
-    public void tearDown() throws Exception {
-        
-    }
-
 
     @Override
     public String getSessionId() {
